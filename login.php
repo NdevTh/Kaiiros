361618d599ec7a2a79d08910e43b8547582afb6a
ct mac <cth.huynh@gmail.com>
2023-08-02 23:47:23 +0200

    update files

---
+++ login.php
@@ -0,0 +1,6 @@
+<?php 
+    /* get current local language */
+    $iso_code = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    include "header.inc.php";    
+    //echo substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+?>
