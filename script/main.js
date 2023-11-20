89618f7e984506f0f411cfdee3508e7a285e8ea8
ct mac <cth.huynh@gmail.com>
2023-11-20 16:12:31 +0100

    add navMenu

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@ import BubblesCreator from './BubblesCreator.js';
 import CountDown from './CountDown.js';// Appeler la fonction pour créer les bulles lors du chargement de la page
 
 const bubblesCreator = new BubblesCreator("bubbles-container");
-bubblesCreator.createBubbles(9, 1);
+bubblesCreator.createBubbles(5, 1);
 
 const countDown = new CountDown("January 01 2024 00:00:00");
 
